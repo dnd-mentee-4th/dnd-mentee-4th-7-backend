d40320494c1fe23686504276a616ad68bc0eb0db
Chaeyun Lee <chaeyunn@iochord.com>
2021-02-06 22:19:01 +0900

    기본세팅

---
+++ moagym/src/main/java/org/dnd/moagym/MoagymApplication.java
@@ -0,0 +1,13 @@
+package org.dnd.moagym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoagymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoagymApplication.class, args);
+	}
+
+}
---
+++ moagym/src/test/java/org/dnd/moagym/MoagymApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dnd.moagym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoagymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
